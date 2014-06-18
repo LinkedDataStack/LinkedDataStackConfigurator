a9bcf1bad79fac8e2d5758d02090b70a48602a34
Paul A. Massey <paul.massey@tenforce.com>
2014-06-18 14:41:15 +0200

    Updated the package versions and the list of packages to be tested.

--- SupportPackages/lodstack_analyse_packages/update_packages.sh
+++ SupportPackages/lodstack_analyse_packages/update_packages.sh
@@ -3,13 +3,22 @@
 #
 # This will update the named packages for checking and building (via sudo)
 
-PACKAGES="sieve silk erfurt r2r lodms lod2webapi lod2-refine lod2statworkbench
+PACKAGES="sieve silk erfurt r2r lod2webapi lod2statworkbench 
           ontowiki-cubeviz ontowiki-csvimport ontowiki-common 
           virtuoso-opensource rdf-dataset-integration ontowiki-virtuoso 
           dbpedia-spotlight-ui dl-learner-components-core lod2demo limes 
-          ore-gwt ore-ui owcli rsine
-          unifiedviews-backend unifiedviews-webapp
-          sigmaee sparqlify sparqled spatial-semantic-browser"
+          ore-ui owcli rsine lod2-virtuoso-opensource liberfurt-php             
+          unifiedviews-backend unifiedviews-webapp jar2deb siren                
+          ontowiki-mysql stanbol sparqlproxy-php silk-latc                      
+          lodstack-analyse-packages pivot-browser libjs-rdfauthor               
+          sparqlify sparqled spatial-semantic-browseri                          
+          libvirtodbc0 libvirtuoso5.5-cil virtuoso-minimal virtuoso-nepomuk     
+          virtuoso-opensource-7.1 virtuoso-opensource                           
+          virtuoso-opensource-7.1-bin virtuoso-opensource-7.1-common            
+          virtuoso-server virtuoso-vad-bpel virtuoso-vad-conductor              
+          virtuoso-vad-demo virtuoso-vad-doc virtuoso-vad-isparql               
+          virtuoso-vad-ods virtuoso-vad-rdfmappers virtuoso-vad-sparqldemo      
+          virtuoso-vad-syncml virtuoso-vad-tutorial virtuoso-vsp-startpager"
 
 for i in ${PACKAGES}
 do
